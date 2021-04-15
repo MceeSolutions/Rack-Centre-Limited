dfdc3429a39245c419a6b007b696ac8d4a409139
Olalekan Babawale <padinality@yahoo.com>
2021-04-15 13:30:00 +0000

    [UPD]rc_business_dev: add notifications for other states

--- rc_business_dev/models/sale.py
+++ rc_business_dev/models/sale.py
@@ -26,13 +26,25 @@ class SaleOrder(models.Model):
         and return the right state when the button is clicked. It will be cleaned later.
         """
         self.check_if_above_limit()
-        state = 'submit'
-        self.state = state
+        message = "Sales Order {} requires your approval".format(self.name)
+        group_senior_sales_manager = self.env['ir.model.data'].xmlid_to_object('rc_business_dev.group_senior_sales_manager')
+        partners_to_notify = self.env['res.partner'].sudo()
+        for user in group_senior_sales_manager.users:
+            partners_to_notify += user.partner_id
+        self.notify_of_sale_order(message=message, partner_ids=partners_to_notify.ids)
+        self.state = 'submit'
+        return True
 
     def senior_manager_approve(self):
         if self.discount_above_limit:
             self.state = 'approve'
         else:
+            message = "Sales Order {} requires your approval".format(self.name)
+            group_elt = self.env['ir.model.data'].xmlid_to_object('rc_business_dev.group_elt')
+            partners_to_notify = self.env['res.partner'].sudo()
+            for user in group_elt.users:
+                partners_to_notify += user.partner_id
+            self.notify_of_sale_order(message=message, partner_ids=partners_to_notify.ids)
             self.action_confirm()
 
     def elt_approve(self):
@@ -49,7 +61,13 @@ class SaleOrder(models.Model):
             partners_to_notify += user.partner_id
         for user in group_finance.users:
             partners_to_notify += user.partner_id
-        self.message_subscribe(partner_ids=partners_to_notify.ids)
-        subject = "Sales Order {} has been approved".format(self.name)
-        self.message_post(subject=subject,body=subject, partner_ids=partners_to_notify.ids)
+        message = "Sales Order {} has been approved for Invoicing".format(self.name)
+        self.notify_of_sale_order(message=message, partner_ids=partners_to_notify.ids)
         self.action_confirm()
+
+    def notify_of_sale_order(self, message=None, partner_ids=[]):
+        if not (message and partner_ids):
+            return
+        self.message_subscribe(partner_ids=partner_ids)
+        self.message_post(subject=subject, body=message, partner_ids=partner_ids)
+        return True
