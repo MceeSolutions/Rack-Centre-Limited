c3e1406857d9623365cd4fc1205bf3726092deee
Olalekan Babawale <padinality@yahoo.com>
2021-04-19 13:47:02 +0000

    [UPD]rc_stock: Check if picking is internal transfer before notifying pm

--- rc_stock/models/stock.py
+++ rc_stock/models/stock.py
@@ -15,9 +15,9 @@ class StockPicking(models.Model):
     def notify_project_manager(self):
         group_project_manager = self.env['ir.model.data'].xmlid_to_object('project.group_project_manager')
         partners_to_notify = self.env['res.partner'].sudo()
-        for user in group_data_center.users:
+        for user in group_project_manager.users:
             partners_to_notify += user.partner_id
         self.message_subscribe(partner_ids=partners_to_notify.ids)
-        subject = "Receipt #{} has been validated".format(self.name)
+        subject = "Receipt {} has been validated".format(self.name)
         self.message_post(subject=subject, body=subject, partner_ids=partners_to_notify.ids)
         return True
