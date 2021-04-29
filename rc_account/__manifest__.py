a296473aca5061cf535522711d243311b0d848c6
Olalekan Babawale <padinality@yahoo.com>
2021-04-29 16:57:41 +0000

    [ADD]rc_account: Add module for account customizations

---
+++ rc_account/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Rack Centre Account Modification",
+
+    'summary': """
+       Modifications to Account Processes for Rack Centre. Changes include:
+       """,
+
+    'description': """
+        Modifications to Account Processes for Rack Centre. Changes include:
+       - Journal Posting Process
+       - Payroll Process
+       - Payroll Process
+       - Payroll Process
+       - Payroll Process
+       - Payroll Process
+       - Payroll Process
+       - Payroll Process
+       - Payroll Process
+       - Payroll Process
+       - Payroll Process
+       - Payroll Process
+       - Payroll Process
+    """,
+
+    'author': "MCEE Business Solutions",
+    'website': "http://www.mceesolutions.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['account', 'account_accountant'],
+
+    'data': [
+        'views/views.xml',
+    ],
+}
