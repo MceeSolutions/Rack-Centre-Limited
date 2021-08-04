ba6e565a75eb58315f5c15d2e211efd996080da9
Steve Olise <solise@mceesolutions.com>
2021-08-04 13:02:10 +0100

    [UPD]rc_crm_lead: added fields to lead form

--- rc_crm_lead/models/models.py
+++ rc_crm_lead/models/models.py
@@ -24,6 +24,7 @@ class Lead(models.Model):
 
     channel_partner = fields.Char(string='Channel Partner')
     source_url = fields.Char(string='Source URL')
+    lead_source = fields.Char(string='Lead Source')
     lead_status = fields.Char(string='Lead Status')
     linkedin_connection_by_sales_team = fields.Char(string='LinkedIn Connection by Sales Team')
     leads_to_pipeline_conversion = fields.Char(string='Leads to Pipeline Conversion')
