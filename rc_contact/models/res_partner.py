c020996d5492a722d2bea094a40f899d591a414c
Steve Olise <solise@mceesolutions.com>
2021-09-19 11:43:42 +0100

    [IMP]rc_contact: added missing fields

--- rc_contact/models/res_partner.py
+++ rc_contact/models/res_partner.py
@@ -7,6 +7,8 @@ class Partner(models.Model):
 
     industry_id = fields.Many2one(comodel_name='res.partner.industry', string='Vertical')
     lead_source = fields.Char(string='Lead Source')
+    linkedin_url = fields.Char(string='LinkedIn URL')
+    linkedin_connection_sales = fields.Char(string='LinkedIn connection by sales team')
     
     import_create_date = fields.Datetime('Creation Date', readonly=False, copy=False)
     import_created_by = fields.Char('Created By', readonly=False, copy=False)
